a175fe7f54539b754d67ff37f6f2a5170124d96f
Max Bruckner <max@maxbruckner.de>
2019-05-24 20:05:06 +0200

    Add .editorconfig

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,59 @@
 pub struct Pairs<IteratorType: Iterator<Item = ItemType>, ItemType: Copy> {
-    iterator: IteratorType,
-    last_item: Option<ItemType>,
+	iterator: IteratorType,
+	last_item: Option<ItemType>,
 }
 
 impl<IteratorType: Iterator<Item = ItemType>, ItemType: Copy> Pairs<IteratorType, ItemType> {
-    fn new(iterator: IteratorType) -> Pairs<IteratorType, ItemType> {
-        Pairs {
-            iterator,
-            last_item: None,
-        }
-    }
+	fn new(iterator: IteratorType) -> Pairs<IteratorType, ItemType> {
+		Pairs {
+			iterator,
+			last_item: None,
+		}
+	}
 }
 
 impl<IteratorType: Iterator<Item = ItemType>, ItemType: Copy> Iterator for Pairs<IteratorType, ItemType> {
-    type Item = (ItemType, ItemType);
+	type Item = (ItemType, ItemType);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        None
-    }
+	fn next(&mut self) -> Option<Self::Item> {
+		None
+	}
 }
 
 pub trait PairIterator {
-    type Item: Copy;
-    type Iterator: Iterator<Item = Self::Item>;
+	type Item: Copy;
+	type Iterator: Iterator<Item = Self::Item>;
 
-    fn pairs(self) -> Pairs<Self::Iterator, Self::Item>;
+	fn pairs(self) -> Pairs<Self::Iterator, Self::Item>;
 }
 
 impl<IteratorType: Iterator<Item = ItemType>, ItemType: Copy> PairIterator for IteratorType {
-    type Item = ItemType;
-    type Iterator = Self;
+	type Item = ItemType;
+	type Iterator = Self;
 
-    fn pairs(self) -> Pairs<Self::Iterator, Self::Item> {
-        Pairs::new(self)
-    }
+	fn pairs(self) -> Pairs<Self::Iterator, Self::Item> {
+		Pairs::new(self)
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::PairIterator;
+	use crate::PairIterator;
 
-    #[test]
-    fn should_provide_nothing_without_items() {
-        let array: [i32; 0] = [];
-        let mut iterator = array.iter().pairs();
+	#[test]
+	fn should_provide_nothing_without_items() {
+		let array: [i32; 0] = [];
+		let mut iterator = array.iter().pairs();
 
-        assert_eq!(None, iterator.next());
-    }
+		assert_eq!(None, iterator.next());
+	}
 
-    #[test]
-    fn should_provide_nothing_for_only_one_input() {
-        let array = [1];
-        let mut iterator = array.iter().pairs();
+	#[test]
+	fn should_provide_nothing_for_only_one_input() {
+		let array = [1];
+		let mut iterator = array.iter().pairs();
 
-        assert_eq!(None, iterator.next());
-    }
+		assert_eq!(None, iterator.next());
+	}
+
 }
